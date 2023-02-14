3c4f836137a3f252cb188affb5e135f69d760478
Nurlan Ormond <sunshineforester@gmail.com>
2023-02-14 16:20:12 +0300

    Second digit of number

---
+++ Task_10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("\nInput Three-Digit number:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a>99 & a<1000)
+{
+  char[] array = a.ToString().ToCharArray();
+  Console.WriteLine("Second Digit of the number is: " + array[1]);
+} 
+else 
+{
+Console.WriteLine("This is not Three-Digit number!");
+}
