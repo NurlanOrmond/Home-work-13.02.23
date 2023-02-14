af1592b4dbfbef45cd02e01edc9a5c46357a9469
Nurlan Ormond <sunshineforester@gmail.com>
2023-02-14 16:21:01 +0300

    Third digit of number

---
+++ Task_13/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 645, b=78, c=32679;
+
+char[] arrA = a.ToString().ToArray();
+char[] arrB = b.ToString().ToArray();
+char[] arrC = c.ToString().ToArray();
+
+if (arrA.Length>=3)Console.WriteLine ($"\nThird Digit of {a} is: " + arrA[2]);
+else Console.WriteLine ("Third Digit is absent");
+
+if (arrB.Length>=3)Console.WriteLine ($"\nThird Digit of {b} is: " + arrB[2]);
+else Console.WriteLine ("\nThird Digit is absent");
+
+if (arrC.Length>=3)Console.WriteLine ($"\nThird Digit of {c} is: " + arrC[2]);
+else Console.WriteLine ("\nThird Digit is absent");
+
