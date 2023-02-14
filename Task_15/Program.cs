9c0e3c387ee495852ed5a25c697b628d3586130b
Nurlan Ormond <sunshineforester@gmail.com>
2023-02-14 16:21:36 +0300

    Day Off

---
+++ Task_15/Program.cs
@@ -0,0 +1,17 @@
+﻿ Console.WriteLine("Input number of week day to find out if it's day off");
+ int a = Convert.ToInt32(Console.ReadLine());
+
+if (a>0 & a<8)
+{
+        if (a==6 || a==7)
+    {
+    System.Console.WriteLine("Yes, it's true!");
+    }
+    else
+    {
+    System.Console.WriteLine("No, it's working day.");
+    }
+}
+else
+System.Console.WriteLine("Wrong number!");
+      
